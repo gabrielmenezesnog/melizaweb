879720e35452e19b36f32c0b97ebba2c355e69f8
Gabriel de Menezes <gabriel.gmnogueira@gmail.com>
2023-03-16 15:55:49 -0300

    feat: implementação do formulário de cadastro

--- src/components/auth/Form/AuthForm.tsx
+++ src/components/auth/Form/AuthForm.tsx
@@ -8,8 +8,10 @@ import styles from './AuthForm.module.css';
 interface Props {}
 
 interface State {
+  nome: string;
   email: string;
   senha: string;
+  repetirSenha: string;
   error: boolean;
   formLogin: boolean;
 }
@@ -18,8 +20,10 @@ class AuthForm extends Component<Props, State> {
   constructor(props: Props) {
     super(props);
     this.state = {
+      nome: '',
       email: '',
       senha: '',
+      repetirSenha: '',
       error: false,
       formLogin: true,
     };
@@ -42,10 +46,18 @@ class AuthForm extends Component<Props, State> {
 
   handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { id, value } = event.target;
+    if (id === 'nome') {
+      this.setState({ nome: value });
+    }
     if (id === 'email') {
       this.setState({ email: value });
-    } else if (id === 'senha') {
+    }
+    if (id === 'senha') {
       this.setState({ senha: value });
+    }
+
+    if (id === 'repetirSenha') {
+      this.setState({ repetirSenha: value });
     }
   };
 
@@ -72,36 +84,34 @@ class AuthForm extends Component<Props, State> {
                   </button>
                 </p>
               </div>
-              {formLogin ? (
-                <form onSubmit={this.handleSubmit}>
-                  <div>
-                    <InputTexto
-                      id="email"
-                      label="email"
-                      value={this.state.email}
-                      onBlur={this.handleBlur}
-                      onChange={this.handleChange}
-                    />
-                  </div>
+              <form onSubmit={this.handleSubmit}>
+                <div>
+                  <InputTexto
+                    id="email"
+                    label="email"
+                    value={this.state.email}
+                    onBlur={this.handleBlur}
+                    onChange={this.handleChange}
+                  />
+                </div>
 
-                  <div>
-                    <InputSenha
-                      id="senha"
-                      label="senha"
-                      value={this.state.senha}
-                      onBlur={this.handleBlur}
-                      onChange={this.handleChange}
-                    />
-                  </div>
+                <div>
+                  <InputSenha
+                    id="senha"
+                    label="senha"
+                    value={this.state.senha}
+                    onBlur={this.handleBlur}
+                    onChange={this.handleChange}
+                  />
+                </div>
 
-                  <div>
-                    <BotaoPrincipal
-                      label="continuar"
-                      onClick={() => console.log('clicou')}
-                    />
-                  </div>
-                </form>
-              ) : ''}
+                <div>
+                  <BotaoPrincipal
+                    label="continuar"
+                    onClick={() => console.log('clicou')}
+                  />
+                </div>
+              </form>
             </div>
           </div>
         ) : (
@@ -119,36 +129,57 @@ class AuthForm extends Component<Props, State> {
                   </button>
                 </p>
               </div>
-              {formLogin ? (
-                <form onSubmit={this.handleSubmit}>
-                  <div>
-                    <InputTexto
-                      id="email"
-                      label="email"
-                      value={this.state.email}
-                      onBlur={this.handleBlur}
-                      onChange={this.handleChange}
-                    />
-                  </div>
+              <form onSubmit={this.handleSubmit}>
+                <div>
+                  <InputTexto
+                    id="nome"
+                    label="nome"
+                    placeholder="nome e sobrenome"
+                    value={this.state.nome}
+                    onBlur={this.handleBlur}
+                    onChange={this.handleChange}
+                  />
+                </div>
+                <div>
+                  <InputTexto
+                    id="email"
+                    label="email"
+                    placeholder="exemplo@email.com"
+                    value={this.state.email}
+                    onBlur={this.handleBlur}
+                    onChange={this.handleChange}
+                  />
+                </div>
 
-                  <div>
-                    <InputSenha
-                      id="senha"
-                      label="senha"
-                      value={this.state.senha}
-                      onBlur={this.handleBlur}
-                      onChange={this.handleChange}
-                    />
-                  </div>
+                <div>
+                  <InputSenha
+                    id="senha"
+                    label="senha"
+                    placeholder="mínimo de 8 caracteres"
+                    value={this.state.senha}
+                    onBlur={this.handleBlur}
+                    onChange={this.handleChange}
+                  />
+                </div>
 
-                  <div>
-                    <BotaoPrincipal
-                      label="continuar"
-                      onClick={() => console.log('clicou')}
-                    />
-                  </div>
-                </form>
-              ) : ''}
+                <div>
+                  <InputSenha
+                    id="repetirSenha"
+                    label="repetir senha"
+                    placeholder="repita a senha"
+                    value={this.state.repetirSenha}
+                    onBlur={this.handleBlur}
+                    onChange={this.handleChange}
+                  />
+                </div>
+
+                <div>
+                  <BotaoPrincipal
+                    label="continuar"
+                    onClick={() => console.log('clicou')}
+                  />
+                </div>
+              </form>
             </div>
           </div>
         )}
